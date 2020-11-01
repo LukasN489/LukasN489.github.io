db3f80e0c02ab9f66753b589a9af12e447499c51
LukasN489 <lukas.nordstrom@sjsu.edu>
2020-10-31 17:25:51 -0700

    changed diyps.js file

--- diy.photoshop/diyps.js
+++ diy.photoshop/diyps.js
@@ -1,20 +1,33 @@
 var img;
-var initials ='tv'; // your initials
+var initials ='tg'; // your initials
 var choice = '1'; // starting choice, so it is not empty
-var screenbg = 250; // off white background
 var lastscreenshot=61; // last screenshot never taken
 
 function preload() {
 // preload() runs once, it may make you wait
-//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
+//  img = loadImage('cat.png');  // cat.jpg needs to be next to this .js file
 // you can link to an image on your github account
-  img = loadImage('shiba.png');
+  img = loadImage('https://hitswi.github.io/images/rabbit2.png');
 }
 
 function setup() {
-createCanvas(1920, 1080);  // canvas size
-background(screenbg);   // use our background screen color
+createCanvas(1200, 900);  // canvas size
+  // Define colors
+  c1 = color(4, 17, 62);
+  c2 = color(121, 25, 97);
+  setGradient(c1, c2);
 
+}
+g
+function setGradient(c1, c2) {
+  // noprotect
+  noFill();
+  for (var y = 0; y < height; y++) {
+    var inter = map(y, 0, height, 0, 1);
+    var c = lerpColor(c1, c2, inter);
+    stroke(c);
+    line(0, y, width, y);
+  }
 }
 
 function draw() {
@@ -25,6 +38,37 @@ function draw() {
   if (mouseIsPressed){
     newkeyChoice(choice);  // if the mouse is pressed call newkeyChoice
   }
+  stroke(0, 0, 0, 0); //white
+  keyTyped(); //type key for brush color
+  // 'v' violet
+  // 'i' light blue
+  // 'p' pink
+  // 'w' white
+  // 'y' yellow
+
+  //type 'c' to clear canvas
+
+  spray(); //canvas click triggers spray
+}
+
+function spray() {
+  let leftWall = 0;
+  let rightWall = 1200;
+  let topWall = 0;
+  let bottomWall = 1200;
+  if (mouseIsPressed) {
+    for (let i = 0; i < 200; i++) {
+      
+      let xc = constrain(mouseX, leftWall, rightWall);
+      let yc = constrain(mouseY, topWall, bottomWall);
+      let x = random(-10, 10);
+      let y = random(-10, 10);
+      if (dist(0, 0, x, y) < 100) {
+        ellipseMode(CENTER);
+        ellipse(xc + x, yc + y, 1, 1);
+      }
+    }
+  }
 }
 
 function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
@@ -33,65 +77,58 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
   // graphic function
 
  if (toolChoice == '1' ) {  // first tool
-    fill(140, 85, 245, 96);
-    circle(mouseX, mouseY, 10, 10);
+   
+    stroke(255, 90);
+    line(mouseX, mouseY, pmouseX, pmouseY);
     
   } else if (toolChoice == '2') { // second tool
-    fill(29, 158, 250, 98);
-    rect(mouseX, mouseY, pmouseX, pmouseY);
-    
+
+    stroke(230, 103, 165, 75);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
   } else if (toolChoice == '3') { // third tool
-    stroke(35, 153, 84, 60);
+
+    stroke(300, 100, 0, 80);
     line(mouseX, mouseY, pmouseX, pmouseY);
-    
   } else if (toolChoice == '4') {
-    fill(250, 154, 1, 98);
-    ellipse(mouseX, mouseY, pmouseX, pmouseY);
-    
-  } else if (key == '5') { // this tool calls a function
-    stroke(100, 56, 218, 96);
-    testbox(50, 252, 150);
-    testbox(200, 50, 252);
-    
-    
-    
-    
-    // make testbox do something!
- //   line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '6') {
-    stroke(0,0,200);
+
+    stroke(0, 0, 255);
     line(mouseX, mouseY, pmouseX, pmouseY);
-    
-  } else if (toolChoice == '7') {
-    fill(245, 191, 243, 96);
-    circle(mouseX, mouseY, 50, 50);
-    
-  } else if (toolChoice == '8') {
-    fill(100, 100, 0, 80);
-    arc(mouseX, mouseY, 50, 50,0, HALF_PI);
-    
-  } else if (toolChoice == '9') {
-    fill(150, 200, 0, 80);
-    arc(mouseX, mouseY, 70, 40, 0, PI+QUARTER_PI);
-    
-  } else if (toolChoice == '0') {
-    stroke(0, 0);
-    fill(random(255), random(255), random(255), random(255));
-    circle(mouseX, mouseY, 50, 50);
-    
+  } else if (key == '5') { // this tool calls a function
+    stroke(255, 255, 255);
+    testbox(20, 20, 200);
   } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    image(img, mouseX, mouseY, 121, 100);
-    
+  image(img, mouseX, mouseY);
+  } else if (toolChoice == 'h' || toolChoice == 'H') { // g places the image we pre-loaded
+  image(img2, mouseX, mouseY);
   }
- }
- 
-function testbox(r, g, b) {
+}
+  
+function keyTyped() {
+  strokeWeight(1);
+  if (key === 'v') {
+    stroke(23, 48, 130, 80); //violet
+  }
+  if (key === 'i') {
+    stroke(150, 226, 255, 80); //light blue
+  }
+  if (key === 'p') {
+    stroke(255, 131, 182, 80); //pink
+  }
+  if (key === 'w') {
+    stroke(255, 255, 255, 80); //white
+  }
+  if (key === 'y') {
+    stroke(255, 255, 0, 80); //yellow
+  }
+}
+  
+function testbox(white) {
 // this is a test function that will show you how you can put your own functions into the sketch
   x = mouseX;
   y = mouseY;
-  fill(r, g, b);
-  ellipse(x-50, y-50, 50, 100);
-
+  fill(255);
+  //rect(x-50, y-50, 10, 10);
 }
 
 function clear_print() {
@@ -99,7 +136,7 @@ function clear_print() {
 // p calls the routine saveme, which saves a copy of the screen
   if (key == 'x' || key == 'X') {
     background(screenbg); // set the screen back to the background color
-  } else if (key == 'p' || key == 'P') {
+  } else if (key == 's' || key == 'S') {
      saveme();  // call saveme which saves an image of the screen
   }
 }
