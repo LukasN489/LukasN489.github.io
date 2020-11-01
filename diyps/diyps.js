a00136a84f0f7bb6cc63280f31a18845837f88df
LukasN489 <lukas.nordstrom@sjsu.edu>
2020-11-01 07:21:58 -0800

    removed brush option

--- diyps/diyps.js
+++ diyps/diyps.js
@@ -98,11 +98,6 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
   } else if (key == '5') { // this tool calls a function
     stroke(255, 255, 255);
     testbox(20, 20, 200);
-  } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-  image(img, mouseX, mouseY);
-  } else if (toolChoice == 'h' || toolChoice == 'H') { // g places the image we pre-loaded
-  image(img2, mouseX, mouseY);
-  }
 }
   
 function keyTyped() {
